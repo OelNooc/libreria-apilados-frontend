6eb737fc9189d628fa4244abbec783f6b3962648
OelNooc <leo.catejoa@gmail.com>
2025-01-25 16:20:57 -0300

    minor fix on import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import AddBook from "./pages/Addbook";
+import AddBook from "./pages/AddBook";
 import ListBooks from "./pages/ListBooks";
 import SearchBooks from "./pages/SearchBooks";
 import BookDetail from "./components/BookDetail";
